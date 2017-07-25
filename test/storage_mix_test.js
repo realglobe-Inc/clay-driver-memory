03c37c6975edbd51eca210f7f9060472541f1bca
Taka Okunishi <okunishinishi@gmail.com>
2017-07-25 09:33:29 +0900

    Fix lock file

---
+++ test/storage_mix_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for storageMix.
+ * Runs with mocha.
+ */
+'use strict'
+
+const storageMix = require('../lib/mixins/storage_mix.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('storage-mix', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Storage mix', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
